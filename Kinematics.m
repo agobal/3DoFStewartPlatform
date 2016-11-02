40887df06f45c081f2b1556167265d7f9350200e
agobal <agobal@gmail.com>
2016-11-02 14:32:26 -0700

    Errors addressed

--- Kinematics.m
+++ Kinematics.m
@@ -18,7 +18,7 @@
 prompt = {'Length (m):', 'Width (m):'};
 dlg_title = 'Platform geometry';
 num_lines = 1;
-defaultans = {'1', '0.5'};
+defaultans = {'0.0833', '0.1'};
 answer = inputdlg(prompt, dlg_title, num_lines, defaultans);
 
 platform_length = str2double(char(answer(1)));
@@ -26,21 +26,21 @@ platform_width = str2double(char(answer(2)));
 
 % Jack properties
 
-prompt = {'Closed length (m):', 'Limit length (m):'};
+prompt = {'Closed length (m):', 'Displacement (m):'};
 dlg_title = 'Jack properties';
 num_lines = 1;
-defaultans = {'0.5', '1'};
+defaultans = {'0.42', '0.35'};
 answer = inputdlg(prompt, dlg_title, num_lines, defaultans);
 
 closed_length = str2double(char(answer(1)));
-limit_length = str2double(char(answer(2)));
+limit_length = str2double(char(answer(2)))+str2double(char(answer(1)));
 
 % Jack base locations
 
 prompt = {'X-1 (m):', 'Y-1 (m):', 'X-2 (m):', 'Y-2 (m):', 'X-3 (m):', 'Y-3 (m):'};
 dlg_title = 'Jack basis';
 num_lines = 1;
-defaultans = {'1', '0', '-1', '0.25', '-1', '-0.25'};
+defaultans = {'0.0733', '0', '0.1', '-0.1', '-0.1', '-0.1'};
 answer = inputdlg(prompt, dlg_title, num_lines, defaultans);
 
 jack1_b = [str2double(char(answer(1))) str2double(char(answer(2))) 0];
@@ -53,7 +53,7 @@ jack3_b = [str2double(char(answer(5))) str2double(char(answer(6))) 0];
 prompt = {'X-1 (m):', 'Y-1 (m):', 'X-2 (m):', 'Y-2 (m):', 'X-3 (m):', 'Y-3 (m):'};
 dlg_title = 'Jack-to-platform connections';
 num_lines = 1;
-defaultans = {'1', '0', '-1', '0.25', '-1', '-0.25'};
+defaultans = {'0.0733', '0', '0.1', '-0.1', '-0.1', '-0.1'};
 answer = inputdlg(prompt, dlg_title, num_lines, defaultans);
 
 jack1_p = [str2double(char(answer(1))) str2double(char(answer(2)))];
@@ -73,38 +73,44 @@ while (error == 1)
     prompt = {'Central elevation (m):', 'Roll angle (deg):', 'Pitch angle (deg):'};
     dlg_title = 'Final platform location';
     num_lines = 1;
-    defaultans = {'0.75', '5', '10'};
+    defaultans = {'0.6', '10', '30'};
     answer = inputdlg(prompt, dlg_title, num_lines, defaultans);
 
     z_final = str2double(char(answer(1)));
     theta = str2double(char(answer(2)))*pi/180;
     phi = str2double(char(answer(3)))*pi/180;
     
-    e1 = jack1_initial(3)+jack1_initial(1)*sin(phi)+jack1_initial(2)*sin(theta);
-    e2 = jack2_initial(3)+jack2_initial(1)*sin(phi)+jack2_initial(2)*sin(theta);
-    e3 = jack3_initial(3)+jack3_initial(1)*sin(phi)+jack3_initial(2)*sin(theta);
+    e1 = z_final+jack1_p(1)*sin(phi)+jack1_p(2)*sin(theta);
+    e2 = z_final+jack2_p(1)*sin(phi)+jack2_p(2)*sin(theta);
+    e3 = z_final+jack3_p(1)*sin(phi)+jack3_p(2)*sin(theta);
     error = 0;
     if (e1 < closed_length) || (e2 < closed_length) || (e3 < closed_length) || (e1 > limit_length) || (e2 > limit_length) || (e3 > limit_length)
         error = 1;
+    else
+        error = 0;
     end
 end
 
 %% Final end locations
 jack1_initial = [jack1_p z_0];
-jack1_final = [jack1_initial(1)*cos(phi) jack1_initial(2)*cos(theta) jack1_initial(3)+jack1_initial(1)*sin(phi)+jack1_initial(2)*sin(theta)];
+jack1_final = [jack1_initial(1)*cos(phi) jack1_initial(2)*cos(theta) z_final+jack1_initial(1)*sin(phi)+jack1_initial(2)*sin(theta)];
 jack1_length = sqrt((jack1_final(1) - jack1_b(1))^2 + (jack1_final(2) - jack1_b(2))^2 + (jack1_final(3) - jack1_b(3))^2);
 
 jack2_initial = [jack2_p z_0];
-jack2_final = [jack2_initial(1)*cos(phi) jack2_initial(2)*cos(theta) jack2_initial(3)+jack2_initial(1)*sin(phi)+jack2_initial(2)*sin(theta)];
+jack2_final = [jack2_initial(1)*cos(phi) jack2_initial(2)*cos(theta) z_final+jack2_initial(1)*sin(phi)+jack2_initial(2)*sin(theta)];
 jack2_length = sqrt((jack2_final(1) - jack2_b(1))^2 + (jack2_final(2) - jack2_b(2))^2 + (jack2_final(3) - jack2_b(3))^2);
 
 jack3_initial = [jack3_p z_0];
-jack3_final = [jack3_initial(1)*cos(phi) jack3_initial(2)*cos(theta) jack3_initial(3)+jack3_initial(1)*sin(phi)+jack3_initial(2)*sin(theta)];
+jack3_final = [jack3_initial(1)*cos(phi) jack3_initial(2)*cos(theta) z_final+jack3_initial(1)*sin(phi)+jack3_initial(2)*sin(theta)];
 jack3_length = sqrt((jack3_final(1) - jack3_b(1))^2 + (jack3_final(2) - jack3_b(2))^2 + (jack3_final(3) - jack3_b(3))^2);
 
-jack1_length
-jack2_length
-jack3_length
+disp('Final displacements of each jack:');
+S=sprintf('Jack 1 displacement: %0.5g meters',(jack1_length - z_0));
+disp(S);
+S=sprintf('Jack 2 displacement: %0.5g meters',(jack2_length - z_0));
+disp(S);
+S=sprintf('Jack 3 displacement: %0.5g meters',(jack3_length - z_0));
+disp(S);
 
 % Plot the movement and base
 xb = [jack1_b(1) jack2_b(1) jack3_b(1)];
@@ -114,7 +120,7 @@ zb = [jack1_b(3) jack2_b(3) jack3_b(3)];
 x0 = [jack1_initial(1) jack2_initial(1) jack3_initial(1)];
 y0 = [jack1_initial(2) jack2_initial(2) jack3_initial(2)];
 z0 = [jack1_initial(3) jack2_initial(3) jack3_initial(3)];
-plot3(1,1,1,-1,-1,0)
+plot3(0.1,0.0,0.7,-0.1,-0.1,0)
 hold on
 
 plot3([xb(1) x0(1)], [yb(1) y0(1)], [zb(1) z0(1)]);
@@ -127,7 +133,7 @@ for i = 1:100
     x = [(x0(1) + i*(jack1_final(1) - x0(1))/100) (x0(2) + i*(jack2_final(1) - x0(2))/100) (x0(3) + i*(jack3_final(1) - x0(3))/100)];
     y = [(y0(1) + i*(jack1_final(2) - y0(1))/100) (y0(2) + i*(jack2_final(2) - y0(2))/100) (y0(3) + i*(jack3_final(2) - y0(3))/100)];
     z = [(z0(1) + i*(jack1_final(3) - z0(1))/100) (z0(2) + i*(jack2_final(3) - z0(2))/100) (z0(3) + i*(jack3_final(3) - z0(3))/100)];
-    plot3(1,1,1,-1,-1,0)
+    plot3(0.1,0.0,0.7,-0.1,-0.1,0)
     hold on
     plot3([xb(1) x(1)], [yb(1) y(1)], [zb(1) z(1)]);
     plot3([xb(2) x(2)], [yb(2) y(2)], [zb(2) z(2)]);
